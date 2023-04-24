2f45065d92d34090b1ad44e2257a701f3ecf5a3f
Lucas Anjos <oargus.g@gmail.com>
2023-04-24 20:40:36 -0300

    Criação do component botão e menu

---
+++ src/components/Botao.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Botao = ({ titulo }) => {
+    return (
+        <TouchableOpacity style={estilos.botao}>
+            <Text style={estilos.texto}>{ titulo }</Text>
+        </TouchableOpacity>
+    );
+}
+
+const estilos = StyleSheet.create({
+    botao: {
+        backgroundColor: "#ff1f00",
+        padding: 10,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: "#cecece",
+    },
+    texto: {
+        color: "#fff",
+        fontSize: "1.5em",
+        textAlign: "center",
+        fontWeight: "bold",
+    },
+});
+
+export default Botao;
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Botao from '../components/Botao'
 
 const Menu = () => {
     return (
-        <View>
-            <Text>Menu</Text>
+        <View style={estilos.container}>
+            <View style={estilos.logo}>
+                <Text style={estilos.titulo}>Meu Diário</Text>
+            </View>
+            <View style={estilos.botoes}>
+                <Botao titulo="Novo Diário" />
+            </View>
         </View>
     );
 }
+
+const estilos = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+    },
+    logo: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    botoes: {
+        flex: 2,
+    },
+    titulo: {
+        fontSize: "3em",
+        fontFamily: "Arial",
+        color: "#ff1f00",
+        fontWeight: "bold",
+    },
+});
 
 export default Menu;
